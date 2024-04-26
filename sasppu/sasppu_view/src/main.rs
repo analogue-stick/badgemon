acc47978f5d413bdd63666d030a6b993750910fc
Molive <moliveofscratch@gmail.com>
2024-04-26 18:01:35 +0100

    makes remote view work

--- sasppu/sasppu_view/src/main.rs
+++ sasppu/sasppu_view/src/main.rs
@@ -1,7 +1,7 @@
 use minifb::{Key, Window, WindowOptions};
 use std::{
-    fs::File,
-    io::{Read, Seek},
+    io::{self, BufWriter, Cursor},
+    net::TcpListener,
 };
 
 fn main() {
@@ -16,18 +16,24 @@ fn main() {
     )
     .expect("Unable to create the window");
 
-    let mut file = File::open("../test.hex").unwrap();
-    let mut before_buf = vec![];
+    let mut before_buf = [0u8; 240 * 240 * 2];
     let mut buffer = [0u32; 240 * 240];
+    let socket = TcpListener::bind("0.0.0.0:1234").unwrap();
+    socket.set_nonblocking(true).unwrap();
     while window.is_open() && !window.is_key_down(Key::Escape) {
-        file.seek(std::io::SeekFrom::Start(0)).unwrap();
-        file.read_to_end(&mut before_buf).unwrap();
-        for (i, x) in buffer.iter_mut().enumerate() {
-            let col: u16 = (before_buf[i * 2] as u16) << 8 | before_buf[i * 2 + 1] as u16;
-            *x = ((((col >> 11) & 0x1F) as u32) << (16 + 3))
-                | ((((col >> 5) & 0x3F) as u32) << (8 + 2))
-                | ((((col >> 0) & 0x1F) as u32) << (0 + 3));
+        {
+            if let Ok((mut stream, _)) = socket.accept() {
+                let mut c = Cursor::new(before_buf);
+                io::copy(&mut stream, &mut c).unwrap();
+                before_buf = c.into_inner();
+                for (i, x) in buffer.iter_mut().enumerate() {
+                    let col: u16 = (before_buf[i * 2] as u16) << 8 | before_buf[i * 2 + 1] as u16;
+                    *x = ((((col >> 11) & 0x1F) as u32) << (16 + 3))
+                        | ((((col >> 5) & 0x3F) as u32) << (8 + 2))
+                        | ((((col >> 0) & 0x1F) as u32) << (0 + 3));
+                }
+            }
+            window.update_with_buffer(&buffer, width, height).unwrap();
         }
-        window.update_with_buffer(&buffer, width, height).unwrap();
     }
 }
