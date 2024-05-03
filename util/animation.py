0bac066de3e199af058a261f01f0a0d9b6dbb411
Molive <moliveofscratch@gmail.com>
2024-05-03 22:24:26 +0100

    Adds cyclic animations and random animations

--- util/animation.py
+++ util/animation.py
@@ -1,16 +1,19 @@
 from asyncio import Event
+import math
 from typing import List, Tuple
 
 class Animation:
-    def __init__(self, length: int=1000) -> None:
+    def __init__(self, length: int=1000, infinite=False) -> None:
         assert(length >= 0)
         self.next: List["Animation"] = []
         self.prev: List["Animation"] = []
+        self.ends: List["Animation"] = []
         self.length: int = length
         self.needed_to_start: int = 0
         self.needed_to_start_bak: int = 0
         self.started: bool = False
         self.ended: bool = False
+        self.infinite: bool = False
 
     def update(self, time: float) -> None:
         pass
@@ -62,6 +65,14 @@ class Animation:
             self.needed_to_start = 0
         self.needed_to_start_bak = self.needed_to_start
         return self
+    
+    def ends(self, next: "Animation") -> "Animation":
+        self.ends.append(next)
+        return self
+    
+    def ended_by(self, next: "Animation") -> "Animation":
+        next.ends.append(self)
+        return self
 
 class AnimationEvent(Animation):
     def __init__(self, event: Event, *args, **kwargs) -> None:
@@ -88,6 +99,53 @@ def slower(start:float=0, end:float=1, x:float=0):
     x += 1
     return lerp(start, end, (x * x * 0.5 * (3.0 - x)) - 1)
 
+# Dave Hoskins
+def hash_without_sine(p:float):
+    p *= .1031
+    p = p - math.trunc(p)
+    p *= p + 33.33
+    p *= p + p
+    return p - math.trunc(p)
+
+def scaled_hash_without_sine(start,end,p):
+    return lerp(start,end,hash_without_sine(lerp(start,end*100,p)))
+
+class AnimCycle(Animation):
+    def fun(time: float) -> float:
+        return time % 1.0
+
+    def __init__(self, other: Animation, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.other = other
+        self.infinite = True
+
+    def reset(self) -> None:
+        self.other.reset()
+        return super().reset()
+    
+    def on_anim_end(self) -> None:
+        self.other.on_anim_end()
+        return super().on_anim_end()
+    
+    def on_anim_start(self) -> None:
+        self.other.on_anim_start()
+        return super().on_anim_start()
+    
+    def update(self, time: float) -> None:
+        self.other.update(self.fun(time))
+        return super().update(time)
+    
+class AnimBounce(AnimCycle):
+    def fun(time: float) -> float:
+        if time % 2.0 < 1:
+            return time % 1.0
+        else:
+            return 1.0 - (time % 1.0)
+
+class AnimSin(AnimCycle):
+    def fun(time: float) -> float:
+        return math.sin(time*math.tau)
+
 class EditorAnim(Animation):
     def fun(start:float,end:float,time:float):
         return start
@@ -113,6 +171,9 @@ class AnimFaster(EditorAnim):
 class AnimSlower(EditorAnim):
     fun = slower
 
+class AnimRandom(EditorAnim):
+    fun = scaled_hash_without_sine
+
 class AnimationCoordinator:
     def __init__(self) -> None:
         self.active: List[Tuple[int,Animation]] = []
@@ -120,18 +181,25 @@ class AnimationCoordinator:
 
     def update(self, delta: int):
         self.time += delta
-        for i, (start, anim) in enumerate(self.active):
+        for start, anim in self.active:
             local_time = (self.time - start) / anim.length
-            if local_time > 1:
-                anim.on_anim_end()
-                self.active.pop(i)
-                for next in anim.next:
-                    next.needed_to_start -= 1
-                    if next.needed_to_start <= 0 and not next.started:
-                        self.trigger(next)
+            if not anim.infinite and local_time > 1:
+                self.end(anim)
             else:
                 anim.update(local_time)
+
+        self.active[:] = [i for i in self.active if not i[1].ended]
 
     def trigger(self, anim: Animation):
         self.active.append((self.time,anim))
         anim.on_anim_start()
+
+    def end(self, anim: Animation):
+        anim.on_anim_end()
+        for next in anim.next:
+            next.needed_to_start -= 1
+            if next.needed_to_start <= 0 and not next.started:
+                self.trigger(next)
+        for ends in anim.ends:
+            if ends.started and not ends.ended:
+                self.end(ends)
