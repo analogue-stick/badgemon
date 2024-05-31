db26c05f4c9989fc20c5b5959984ac61da7136a0
Molive <moliveofscratch@gmail.com>
2024-05-31 18:32:36 +0100

    Fix Save Location

--- config.py
+++ config.py
@@ -1,7 +1,12 @@
-import sys
+import sys, os
 if sys.implementation.name == "micropython":
-    ASSET_PATH = "/apps/badgemon/assets/"
-    SAVE_PATH = "/apps/badgemon/saves/"
+    apps = os.listdir("/apps")
+    path = ""
+    for app in apps:
+        if app.startswith("analogue-stick-badgemon-"):
+            path = "/apps/" + app
+    ASSET_PATH = path + "/assets/"
+    SAVE_PATH = path + "/saves/"
 else:
     ASSET_PATH = "./apps/badgemon/assets/"
     SAVE_PATH = "./apps/badgemon/saves/"
