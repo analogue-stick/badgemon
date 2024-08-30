0e68ec4eb429786f68f93f3ecc712f20cbc2b75a
Molive <moliveofscratch@gmail.com>
2024-08-30 22:11:29 +0100

    Implement all store functions

--- sasppu/sasppu_emu/src/simd.rs
+++ sasppu/sasppu_emu/src/simd.rs
@@ -1,5 +1,5 @@
 //Library to make rust functions of the ESP32S3's SIMD instructions.v
-use log::{debug, warn};
+use log::{debug, trace, warn};
 use std::simd::{prelude::*, ToBytes};
 
 pub struct ESP32S3 {
@@ -105,7 +105,7 @@ fn validate_unsigned_mask(imm: u32, mask: u32) {
 }
 
 fn load128(buf: &[u16x8], ac: usize) -> u16x8 {
-    debug!("load128 a{}", ac);
+    trace!("load128 a{}", ac);
     if let Some(val) = buf.get(ac >> 4) {
         debug!("loaded {:?}", val);
         *val
@@ -119,7 +119,7 @@ fn load128(buf: &[u16x8], ac: usize) -> u16x8 {
 }
 
 fn store128(buf: &mut [u16x8], val: u16x8, ac: usize) {
-    debug!("store128 a{}", ac);
+    trace!("store128 a{}", ac);
     if let Some(n_val) = buf.get_mut(ac >> 4) {
         *n_val = val;
         debug!("stored {:?}", val);
@@ -133,9 +133,9 @@ fn store128(buf: &mut [u16x8], val: u16x8, ac: usize) {
 }
 
 fn load64(buf: &[u16x8], ac: usize) -> u16x4 {
-    debug!("load64 a{}", ac);
+    trace!("load64 a{}", ac);
     if let Some(&val) = buf.get(ac >> 4) {
-        let res = if ac & 0b100 > 0 {
+        let res = if ac & 0b1000 > 0 {
             val.rotate_elements_right::<4>()
         } else {
             val
@@ -153,18 +153,136 @@ fn load64(buf: &[u16x8], ac: usize) -> u16x4 {
 }
 
 fn store64(buf: &mut [u16x8], val: u16x4, ac: usize) {
-    debug!("store64 a{}", ac);
+    trace!("store64 a{}", ac);
     if let Some(n_val) = buf.get_mut(ac >> 4) {
         let large = val.resize::<8>(0);
-        if ac & 0b100 > 0 {
-            *n_val = simd_swizzle!(n_val, large, [8, 9, 10, 11, 4, 5, 6, 7]);
+        if ac & 0b1000 > 0 {
+            *n_val = simd_swizzle!(*n_val, large, [8, 9, 10, 11, 4, 5, 6, 7]);
         } else {
-            *n_val = simd_swizzle!(n_val, large, [8, 9, 10, 11,]);
+            *n_val = simd_swizzle!(*n_val, large, [0, 1, 2, 3, 8, 9, 10, 11]);
         }
         debug!("stored {:?}", val);
     } else {
         panic!(
-            "OUT OF BOUNDS: tried store128 to address {} in array of length {}.",
+            "OUT OF BOUNDS: tried store64 to address {} in array of length {}.",
+            ac >> 4,
+            buf.len()
+        )
+    }
+}
+
+fn load32(buf: &[u16x8], ac: usize) -> u16x2 {
+    trace!("load32 a{}", ac);
+    if let Some(&val) = buf.get(ac >> 4) {
+        let mut val = val;
+        for _ in 0..(ac & 0b1100) >> 2 {
+            val = val.rotate_elements_right::<2>();
+        }
+        let res = val.resize::<2>(0);
+        debug!("loaded {:?}", res);
+        res
+    } else {
+        panic!(
+            "OUT OF BOUNDS: tried load32 from address {} in array of length {}.",
+            ac >> 4,
+            buf.len()
+        )
+    }
+}
+
+fn store32(buf: &mut [u16x8], val: u16x2, ac: usize) {
+    trace!("store32 a{}", ac);
+    if let Some(n_val) = buf.get_mut(ac >> 4) {
+        let large = val.resize::<8>(0);
+        match (ac & 0b1100) >> 2 {
+            0 => {
+                *n_val = simd_swizzle!(*n_val, large, [8, 9, 2, 3, 4, 5, 6, 7]);
+            }
+            1 => {
+                *n_val = simd_swizzle!(*n_val, large, [0, 1, 8, 9, 4, 5, 6, 7]);
+            }
+            2 => {
+                *n_val = simd_swizzle!(*n_val, large, [0, 1, 2, 3, 8, 9, 6, 7]);
+            }
+            3 => {
+                *n_val = simd_swizzle!(*n_val, large, [0, 1, 2, 3, 4, 5, 8, 9]);
+            }
+            _ => unreachable!(),
+        }
+        debug!("stored {:?}", val);
+    } else {
+        panic!(
+            "OUT OF BOUNDS: tried store32 to address {} in array of length {}.",
+            ac >> 4,
+            buf.len()
+        )
+    }
+}
+
+fn load16(buf: &[u16x8], ac: usize) -> u16 {
+    trace!("load16 a{}", ac);
+    if let Some(&val) = buf.get(ac >> 4) {
+        let pos = (ac & 0b1110) >> 1;
+        let res = val[pos];
+        debug!("loaded {:?}", res);
+        res
+    } else {
+        panic!(
+            "OUT OF BOUNDS: tried load16 from address {} in array of length {}.",
+            ac >> 4,
+            buf.len()
+        )
+    }
+}
+
+fn store16(buf: &mut [u16x8], val: u16, ac: usize) {
+    trace!("store16 a{}", ac);
+    if let Some(n_val) = buf.get_mut(ac >> 4) {
+        let pos = (ac & 0b1110) >> 1;
+        n_val[pos] = val;
+        debug!("stored {:?}", val);
+    } else {
+        panic!(
+            "OUT OF BOUNDS: tried store16 to address {} in array of length {}.",
+            ac >> 4,
+            buf.len()
+        )
+    }
+}
+
+fn load8(buf: &[u16x8], ac: usize) -> u8 {
+    trace!("load8 a{}", ac);
+    if let Some(&val) = buf.get(ac >> 4) {
+        let pos = (ac & 0b1110) >> 1;
+        let res = if ac & 1 > 0 {
+            (val[pos] >> 8) as u8
+        } else {
+            (val[pos] & 0xFF) as u8
+        };
+        debug!("loaded {:?}", res);
+        res
+    } else {
+        panic!(
+            "OUT OF BOUNDS: tried load8 from address {} in array of length {}.",
+            ac >> 4,
+            buf.len()
+        )
+    }
+}
+
+fn store8(buf: &mut [u16x8], val: u8, ac: usize) {
+    trace!("store8 a{}", ac);
+    if let Some(n_val) = buf.get_mut(ac >> 4) {
+        let pos = (ac & 0b1110) >> 1;
+        if ac & 1 > 0 {
+            n_val[pos] = (n_val[pos] & 0xFF) | ((val as u16) << 8);
+        } else {
+            n_val[pos] = (n_val[pos] & 0xFF00) | (val as u16);
+        }
+        debug!("stored {:?}", val);
+    } else {
+        panic!(
+            "OUT OF BOUNDS: tried store8 to address {} in array of length {}.",
             ac >> 4,
             buf.len()
         )
@@ -173,33 +291,33 @@ fn store64(buf: &mut [u16x8], val: u16x4, ac: usize) {
 
 impl ESP32S3 {
     fn load128(&mut self, buf: &[u16x8], qu: Reg, ac: Reg) {
-        debug!("load128 q{}, a{}", qu, ac);
+        trace!("load128 q{}, a{}", qu, ac);
         validate_qreg(qu);
         validate_areg(ac);
         self.qreg[qu] = load128(buf, self.areg[ac] as usize)
     }
 
     fn load128incp(&mut self, buf: &[u16x8], qu: Reg, ac: Reg) {
-        debug!("load128incp q{}, a{}", qu, ac);
+        trace!("load128incp q{}, a{}", qu, ac);
         self.load128(buf, qu, ac);
         self.areg[ac] = self.areg[ac].wrapping_add(16);
     }
 
     fn store128(&mut self, buf: &mut [u16x8], qv: Reg, ac: Reg) {
-        debug!("store128 q{}, a{}", qv, ac);
+        trace!("store128 q{}, a{}", qv, ac);
         validate_qreg(qv);
         validate_areg(ac);
         store128(buf, self.qreg[qv], self.areg[ac] as usize)
     }
 
     fn store128incp(&mut self, buf: &mut [u16x8], qv: Reg, ac: Reg) {
-        debug!("store128incp q{}, a{}", qv, ac);
+        trace!("store128incp q{}, a{}", qv, ac);
         self.store128(buf, qv, ac);
         self.areg[ac] = self.areg[ac].wrapping_add(16);
     }
 
     pub fn andq(&mut self, qa: Reg, qx: Reg, qy: Reg) {
-        debug!("EE.ANDQ q{}, q{}, q{}", qa, qx, qy);
+        trace!("EE.ANDQ q{}, q{}, q{}", qa, qx, qy);
         validate_qreg(qa);
         validate_qreg(qx);
         validate_qreg(qy);
@@ -207,7 +325,7 @@ impl ESP32S3 {
     }
 
     pub fn bitrev(&mut self, qa: Reg, ac: Reg) {
-        debug!("EE.BITREV q{}, a{}", qa, ac);
+        trace!("EE.BITREV q{}, a{}", qa, ac);
         validate_qreg(qa);
         validate_areg(ac);
         let mut temp =
@@ -219,7 +337,7 @@ impl ESP32S3 {
     }
 
     pub fn cmul_s16(&mut self, qz: Reg, qx: Reg, qy: Reg, sel4: u8) {
-        debug!("EE.CMUL.S16 q{}, q{}, q{}, sel{}", qz, qx, qy, sel4);
+        trace!("EE.CMUL.S16 q{}, q{}, q{}, sel{}", qz, qx, qy, sel4);
         validate_sel(sel4, 4);
         validate_qreg(qz);
         validate_qreg(qx);
@@ -261,9 +379,14 @@ impl ESP32S3 {
         qy: Reg,
         sel4: u8,
     ) {
-        debug!(
+        trace!(
             "EE.CMUL.S16.LD.INCP q{}, a{}, q{}, q{}, q{}, sel{}",
-            qu, ac, qz, qx, qy, sel4
+            qu,
+            ac,
+            qz,
+            qx,
+            qy,
+            sel4
         );
         validate_qreg(qu);
         validate_areg(ac);
@@ -282,9 +405,14 @@ impl ESP32S3 {
         qy: Reg,
         sel4: u8,
     ) {
-        debug!(
+        trace!(
             "EE.CMUL.S16.ST.INCP q{}, a{}, q{}, q{}, q{}, sel{}",
-            qv, ac, qz, qx, qy, sel4
+            qv,
+            ac,
+            qz,
+            qx,
+            qy,
+            sel4
         );
         validate_qreg(qv);
         validate_areg(ac);
@@ -294,7 +422,7 @@ impl ESP32S3 {
     }
 
     pub fn ld_128_usar_ip(&mut self, buf: &[u16x8], qu: Reg, ac: Reg, imm: i32) {
-        debug!("EE.LD.128.USAR.IP q{}, a{}, imm{}", qu, ac, imm);
+        trace!("EE.LD.128.USAR.IP q{}, a{}, imm{}", qu, ac, imm);
         validate_qreg(qu);
         validate_areg(ac);
         validate_signed_immediate_by_bits(imm, 12);
@@ -305,7 +433,7 @@ impl ESP32S3 {
     }
 
     pub fn ld_128_usar_xp(&mut self, buf: &[u16x8], qu: Reg, ac: Reg, ad: Reg) {
-        debug!("EE.LD.128.USAR.XP q{}, a{}, a{}", qu, ac, ad);
+        trace!("EE.LD.128.USAR.XP q{}, a{}, a{}", qu, ac, ad);
         validate_qreg(qu);
         validate_areg(ac);
         validate_areg(ad);
@@ -315,7 +443,7 @@ impl ESP32S3 {
     }
 
     pub fn ld_accx_ip(&mut self, buf: &[u16x8], ac: Reg, imm: i32) {
-        debug!("EE.LD.ACCX.IP a{}, imm{}", ac, imm);
+        trace!("EE.LD.ACCX.IP a{}, imm{}", ac, imm);
         validate_areg(ac);
         validate_signed_immediate_by_bits(imm, 11);
         validate_signed_mask(imm, 0xFFFFFFF8u32 as i32);
@@ -329,7 +457,7 @@ impl ESP32S3 {
     }
 
     /*pub fn ls_qacc_h_h_32_ip(&mut self, buf: &[u16x8], ac: Reg, imm: i32) {
-        debug!("EE.LD.QACC_H.H.32.IP a{}, imm{}", ac, imm);
+        trace!("EE.LD.QACC_H.H.32.IP a{}, imm{}", ac, imm);
         validate_areg(ac);
         validate_signed_immediate_by_bits(imm, 10);
         validate_signed_mask(imm, 0xFFFFFFFCu32 as i32);
@@ -343,7 +471,7 @@ impl ESP32S3 {
     }*/
 
     pub fn ld_ua_state_ip(&mut self, buf: &[u16x8], ac: Reg, imm: i32) {
-        debug!("EE.LD.UA_STATE.IP a{}, imm{}", ac, imm);
+        trace!("EE.LD.UA_STATE.IP a{}, imm{}", ac, imm);
         validate_areg(ac);
         validate_signed_immediate_by_bits(imm, 12);
         validate_signed_mask(imm, 0xFFFFFFF0u32 as i32);
@@ -407,9 +535,14 @@ impl ESP32S3 {
         ac: Reg,
         imm: i32,
     ) {
-        debug!(
+        trace!(
             "EE.LDF.128.IP fu{}, fu{}, fu{}, fu{}, a{}, imm{}",
-            fu3, fu2, fu1, fu0, ac, imm
+            fu3,
+            fu2,
+            fu1,
+            fu0,
+            ac,
+            imm
         );
         validate_areg(ac);
         validate_freg(fu0);
